df84e797a2314df4a5fd161ded783f532cd8ac1b
KaterynaZhuvak <129985868+KaterynaZhuvak@users.noreply.github.com>
2024-01-15 19:29:57 -0500

    updated

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const isValidId = require("../../middleWares/isValidId");
-const { schemas } = require("../../models/user");
-const { register } = require("../../controllers/auth");
+const { register, login } = require("../../controllers/auth");
 
 const router = express.Router();
 
 router.post("/register", register);
+
+router.post("/login", login);
 
 module.exports = router;
